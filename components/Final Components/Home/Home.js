1472249bcd9ae719236b1a09bb39ea33422f397b
harmanSandhu-glitch <harmanjotsingh200310@gmail.com>
2024-09-02 11:36:56 +0530

    2-09-2024

--- components/Final Components/Home/Home.js
+++ components/Final Components/Home/Home.js
@@ -1,102 +1,113 @@
-const gridImages = document.querySelectorAll('.gridImage');
-gridImages.forEach(image => {
-  image.addEventListener('mouseover', () => {
+const gridImages = document.querySelectorAll(".gridImage");
+gridImages.forEach((image) => {
+  image.addEventListener("mouseover", () => {
     gsap.to(image, {
       scale: 1.1,
       duration: 0.8,
-      ease: 'power1.inOut',
+      ease: "power1.inOut",
     });
   });
-  image.addEventListener('mouseleave', () => {
+  image.addEventListener("mouseleave", () => {
     gsap.to(image, {
       scale: 1,
       duration: 0.3,
-      ease: 'power1.inOut',
+      ease: "power1.inOut",
     });
   });
 });
 
-
 const cards = [
-  { card: document.querySelector('#card1'), circle: document.querySelector('#circle1') , check: document.querySelector('#check1') },
-  { card: document.querySelector('#card2'), circle: document.querySelector('#circle2') , check: document.querySelector('#check2') },
-  { card: document.querySelector('#card3'), circle: document.querySelector('#circle3') , check: document.querySelector('#check3') }
+  {
+    card: document.querySelector("#card1"),
+    circle: document.querySelector("#circle1"),
+    check: document.querySelector("#check1"),
+  },
+  {
+    card: document.querySelector("#card2"),
+    circle: document.querySelector("#circle2"),
+    check: document.querySelector("#check2"),
+  },
+  {
+    card: document.querySelector("#card3"),
+    circle: document.querySelector("#circle3"),
+    check: document.querySelector("#check3"),
+  },
 ];
 
 let activeCard = null;
 
 function setActiveCard(index) {
   if (activeCard !== null) {
-      cards[activeCard].card.style.border = '2px solid #212327';
-      cards[activeCard].circle.style.backgroundColor = 'black';
-      cards[activeCard].circle.style.display = '';
-      cards[activeCard].check.style.display = '';
+    cards[activeCard].card.style.border = "2px solid #212327";
+    cards[activeCard].circle.style.backgroundColor = "black";
+    cards[activeCard].circle.style.display = "";
+    cards[activeCard].check.style.display = "";
   }
 
   if (activeCard === index) {
-      activeCard = null;
+    activeCard = null;
   } else {
-      cards[index].card.style.border = '2px solid #E0218A';
-      cards[index].circle.style.backgroundColor = 'black';
-      cards[index].circle.style.display = 'block';
-      cards[index].check.style.display = 'block';
-      activeCard = index;
+    cards[index].card.style.border = "2px solid #E0218A";
+    cards[index].circle.style.backgroundColor = "black";
+    cards[index].circle.style.display = "block";
+    cards[index].check.style.display = "block";
+    activeCard = index;
   }
 }
 
 cards.forEach((item, index) => {
-  item.card.addEventListener('click', () => setActiveCard(index));
+  item.card.addEventListener("click", () => setActiveCard(index));
 });
 
-const card1 = document.querySelector('#card1');
-const card2 = document.querySelector('#card2');
-const card3 = document.querySelector('#card3');
+const card1 = document.querySelector("#card1");
+const card2 = document.querySelector("#card2");
+const card3 = document.querySelector("#card3");
 
-card1.addEventListener('mouseover', () => {
+card1.addEventListener("mouseover", () => {
   gsap.to(card1, {
     scale: 1.02,
     duration: 0.3,
-    ease: 'power1.inOut',
+    ease: "power1.inOut",
   });
 });
 
-card1.addEventListener('mouseleave', () => {
+card1.addEventListener("mouseleave", () => {
   gsap.to(card1, {
     scale: 1,
     duration: 0.3,
-    ease: 'power1.inOut',
+    ease: "power1.inOut",
   });
 });
 
-card2.addEventListener('mouseover', () => {
+card2.addEventListener("mouseover", () => {
   gsap.to(card2, {
     scale: 1.02,
     duration: 0.3,
-    ease: 'power1.inOut',
+    ease: "power1.inOut",
   });
 });
 
-card2.addEventListener('mouseleave', () => {
+card2.addEventListener("mouseleave", () => {
   gsap.to(card2, {
     scale: 1,
     duration: 0.3,
-    ease: 'power1.inOut',
+    ease: "power1.inOut",
   });
 });
 
-card3.addEventListener('mouseover', () => {
+card3.addEventListener("mouseover", () => {
   gsap.to(card3, {
     scale: 1.02,
     duration: 0.3,
-    ease: 'power1.inOut',
+    ease: "power1.inOut",
   });
 });
 
-card3.addEventListener('mouseleave', () => {
+card3.addEventListener("mouseleave", () => {
   gsap.to(card3, {
     scale: 1,
     duration: 0.3,
-    ease: 'power1.inOut',
+    ease: "power1.inOut",
   });
 });
 
---
+++ components/Final Components/Home/Home2.js
@@ -0,0 +1,35 @@
+const collection1 = [
+  {
+    img: "images/13.webp",
+    heading: "Punjab Kurta & Pyjama",
+  },
+  {
+    img: "images/14.webp",
+    heading: "Salwar Suit",
+  },
+  {
+    img: "images/15.webp",
+    heading: "Sharara Suit",
+  },
+  {
+    img: "images/16.webp",
+    heading: "BandhGala Suit",
+  },
+];
+const collectionContainer = document.querySelector(".collectionContainer");
+
+collection1.map((item) => {
+  const div = document.createElement("div");
+  div.classList.add("grid-item");
+  div.innerHTML = `   
+            <img src=${item.img} class="gridImage" alt="" />
+            <div class="btnContainer">
+              <h1 class="gridImageHeading">${item.heading}</h1>
+              <button class="btn btn-3 hover-slide-right">
+                <span>VIEW MORE</span>
+              </button>
+            </div>
+        `;
+  collectionContainer.appendChild(div);
+  console.log(item.img);
+});
